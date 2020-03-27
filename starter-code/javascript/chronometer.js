20ab9277d036c0344abdfebefc00e296a296631a
Àlex <alexmanye@gmail.com>
2020-03-27 22:02:45 +0100

    first commit

---
+++ starter-code/javascript/chronometer.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// Constructor
+class Chronometer{
+  constructor(){
+    this.currentTime = 0;
+    this.intervalId = 0;    
+  };
+
+  startClick(){
+    this.intervalId = setInterval (() => {
+    this.currentTime++;
+    this.setTime();
+    }, 1000);   
+  };
+
+  setMinutes() {
+    return Math.floor(this.currentTime / 60);
+  };
+
+  setSeconds() {
+    return this.currentTime % 60;
+  };
+
+  twoDigitsNumber (number){
+    return (number <  10) ? `0${number}`.toString() : number.toString();
+  };
+
+  setTime () {
+    let minutes = this.twoDigitsNumber(this.setMinutes());
+    let seconds = this.twoDigitsNumber(this.setSeconds());
+    return `${minutes}:${seconds}`;
+  };
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  };
+
+  resetClick() {
+    this.currentTime = 0;
+  };
+};
